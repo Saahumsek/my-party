d9df02f8b77ee3d1b0c273389df6a0181c9ffd1e
Denis Khuon <denis.khuon@gmail.com>
2016-11-22 12:24:45 +0100

    booking completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
 //= require_tree .
+
+$('#sandbox-container input').datepicker({
+    format: "dd/mm/yyyy",
+    weekStart: 1,
+    daysOfWeekHighlighted: "0,6"
+});

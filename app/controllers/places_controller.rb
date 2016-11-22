e5e701ea12e51e61a911982c96313248e7959d2d
Denis Khuon <denis.khuon@gmail.com>
2016-11-22 10:35:32 +0100

    model place completed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,9 @@
 class PlacesController < ApplicationController
 
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
+
   def index
-    
+    @places = Place.all
   end
 
   def new
@@ -12,29 +14,34 @@ class PlacesController < ApplicationController
     @place = Place.new(place_params)
     @place.user = current_user
     if @place.save
-      redirect_to
+      redirect_to place_path(@place)
     else
       render :new
     end
   end
 
   def show
-    
+
   end
 
   def edit
-    
+
   end
 
   def update
-    
+    @place.update(place_params)
+    redirect_to place_path(@place)
   end
 
   def destroy
-    
+
   end
 
   private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
 
   def place_params
     params.require(:place).permit(:name, :address, :capacity, :price)
